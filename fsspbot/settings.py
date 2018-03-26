0a13c34319f960b15f68e8e49a8b7a9835b36967
chiefexb <chiefexb@github.com>
2018-03-26 20:03:15 +0300

    1

--- fsspbot/settings.py
+++ fsspbot/settings.py
@@ -76,10 +76,13 @@ WSGI_APPLICATION = 'fsspbot.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': path4,
+        },
     }
 }
+
 
 
 # Password validation
