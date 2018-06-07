6bb7bdfb25cd32f9a7407990508b2f4ac9bed454
chiefexb <chief_exb@mail.ru>
2018-06-08 00:23:56 +0300

    1

--- jobs.py
+++ jobs.py
@@ -18,11 +18,14 @@ def main():
     p = MyData('/home/f.cnf', 'fsspbotdb')
     setting = p.get_table('setting')
     setting.filter(valuename='FSSPTOKEN')
-    val = setting.values()[0][1]
-    print(val)
+    fssp_token = setting.values()[0][1]
+    fssp = FsspApi(fssp_token)
+    fssp.set_firstname('Сергей')
+    fssp.set_lastname('Шило')
+    tsk = fssp.search_phisycal()
     job = p.get_table('job')
     # while 1:
-    print(job.values())
+    print(tsk)
 
 #p=Setting.objects.filter(valuename="VIBERTOKEN")
 #             tok=p.values()[0]['value']
