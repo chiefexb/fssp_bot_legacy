6eddf3632a1a6d051319f14cb8e0e3f53e7b90af
chiefexb <chief_exb@mail.ru>
2018-06-08 01:38:18 +0300

    1

---
+++ job_service.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+#coding: utf8
+import time
+from datetime import datetime
+from mysqlapi import *
+
+def start_job(**kwargs):
+    p = MyData('/home/f.cnf', 'fsspbotdb')
+    job = p.get_table('job')
+    dt=datetime.now()
+    job.add(start_date=dt, job_name='search_ip', status=1)
+    job.save()
+
+def main():
+    # start search api
+    start_job()
+
+if__name__ == "__main__":
+
+    main()
