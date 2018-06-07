5bdfe041632f6f66fe0fff14247a5cc3293ef4fa
chiefexb <chief_exb@mail.ru>
2018-06-08 01:39:24 +0300

    1

--- job_service.py
+++ job_service.py
@@ -4,6 +4,7 @@ import time
 from datetime import datetime
 from mysqlapi import *
 
+
 def start_job(**kwargs):
     p = MyData('/home/f.cnf', 'fsspbotdb')
     job = p.get_table('job')
@@ -11,10 +12,12 @@ def start_job(**kwargs):
     job.add(start_date=dt, job_name='search_ip', status=1)
     job.save()
 
+
 def main():
     # start search api
     start_job()
 
-if__name__ == "__main__":
+
+if __name__ == "__main__":
 
     main()
