0924b56aec4a9b7f2c9dac3159a3b42e6c68e9f7
chiefexb <chief_exb@mail.ru>
2018-06-12 00:00:15 +0300

    1

---
+++ gerenciador.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+#coding: utf8
+
+import time
+from datetime import datetime
+from mysqlapi import *
+class Worker():
+
+    def __init__(self, *args):
+        pass
+
+    
+
+
+class Client():
+    def __init__(self):
+        pass
+
+    def servidor(self):
+        pass
+
