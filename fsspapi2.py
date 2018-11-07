436dd6f1c97b9159f860b43e5d2eec2f4fe81138
chiefexb <chief_exb@mail.ru>
2018-11-07 18:51:15 +0300

    Add files via upload

---
+++ fsspapi2.py
@@ -0,0 +1,44 @@
+https://habr.com/post/314606/
+import aiohttp
+import asyncio
+
+async def fetch(client):
+    async with client.get('http://python.org') as resp:
+        assert resp.status == 200
+        return await resp.text()
+
+async def main():
+    async with aiohttp.ClientSession() as client:
+        html = await fetch(client)
+        print(html)
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
+
+  base_url="https://api-ip.fssprus.ru/api/v1.0"
+    region=''
+    lastname=''
+    firstname=''
+    r_status_code=''
+    status=-1
+    answ=''
+    result=''
+    task=''
+    token=''
+async with aiohttp.ClientSession() as session:
+    async with session.get(base_url+'/search/physical', params= {'token'    :self.token,
+         'region'   :self.region,
+         'firstname':self.firstname,
+         'lastname' :self.lastname}) as resp
+         if resp.status==200:
+             await  jsdata=resp.json()
+             task=jsdata['response']['task']
+             async with aiohttp.ClientSession() as session:
+             
+async with session.get(base_url+'/status', params= params=
+        {'token':self.token,
+        'task':self.task})    as resp
+         if resp.status==200:
+             await  jsdata=resp.json()
+             status=jsdata['response']['status']
+             
