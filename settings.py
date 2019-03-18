867b79f2c8eb1c64bfbd31e64eb4d8c2e7077063
Alex Pricker <alex.pricker@gmail.com>
2019-03-18 11:09:50 +0000

    F

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ import pathlib
 import yaml
 
 BASE_DIR = pathlib.Path(__file__).parent.parent
-config_path = BASE_DIR + '../fssp.yaml'
+config_path = BASE_DIR / 'bot.yaml'
 
 def get_config(path):
     with open(path) as f:
