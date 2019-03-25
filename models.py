c05f6f4eddac5fade632f4a0aca07b3317e88890
Aleksey Shilo <chief_exb@mail.ru>
2019-03-26 01:30:31 +0300

    FFF

--- models.py
+++ models.py
@@ -26,6 +26,21 @@ fact = Table(
 )
 
 
+async def get_user_session(conn, user_id):
+
+    result = await conn.execute(
+        user_session.select()
+        .where(user_session.c.user_id == user_id))
+    user_session_record = await result.first()
+    if not user_session_record:
+        await conn.execute(user_session.insert().values(user_id=user_id, intent_name='start'))
+    result = await conn.execute(
+        user_session.select()
+        .where(user_session.c.user_id == user_id))
+    user_session_record = await result.first()
+    return user_session_record
+
+
 async def add_fact(conn, user_id, fact_name, fact_value):
     result = await conn.execute(
         fact.select()
@@ -33,7 +48,7 @@ async def add_fact(conn, user_id, fact_name, fact_value):
     fact_record = await result.first()
     if not fact_record:
         await conn.execute(fact.insert().values(user_id=user_id, fact_name=fact_name), fact_value=fact_value)
-    
+
 
 
 
