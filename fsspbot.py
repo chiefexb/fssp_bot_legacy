b17f6c27b72ee31988219fc02a0fbdefd9e38c5b
Aleksey Shilo <chief_exb@mail.ru>
2019-03-26 01:52:51 +0300

    FFF

--- fsspbot.py
+++ fsspbot.py
@@ -60,9 +60,8 @@ async def handler(request):
 
     if len(fact_name)>0 :
         fact_value = data['message']['text']
-        logging.info(u'fact'+str(data['message']))
+        logging.info(u'fact'+str(data['message']['text']))
         async with request.app['db'].acquire() as conn:
-
             await  add_fact(conn, user_id,fact_name=fact_name, fact_value=fact_value)
         async with request.app['db'].acquire() as conn:
             result = await conn.execute(
